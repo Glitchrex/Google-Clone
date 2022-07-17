eee57cced8bf6078ef2831f9faf08a55278797bd
Tushar <singhtushar348@gmail.com>
2022-07-17 09:13:48 +0100

    Add search from enter key

--- cstm.js
+++ cstm.js
@@ -1,51 +1,48 @@
-const sInput=document.getElementById("sbtn");
-const lInput=document.getElementById("lbtn");
-
 function loc(){
-if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(showpos);
-} else{
-    alert('Browser not supported')
-}
-function showpos(position){
-    console.log(position.coords.latitude);
-    console.log(position.coords.longitude);
-var api=( "http://maps.googleapis.com/maps/api/geocode/json?"+ position.coords.latitude + "," + position.coords.longitude);
-                    console.log(api);
-               document.getElementById("a").innerhtml=location.href="https://maps.google.com?q="+ position.coords.latitude + "," + position.coords.longitude;
-}
-}
-
-
-function scrch(){
-    
+  if(navigator.geolocation){
+  navigator.geolocation.getCurrentPosition(showpos);
+  } else{
+  alert('Browser not supported')
+  }
+  function showpos(position){
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+  var api=( "http://maps.googleapis.com/maps/api/geocode/json?"+ position.coords.latitude + "," + position.coords.longitude);
+  console.log(api);
+  document.getElementById("a").innerhtml=location.href="https://maps.google.com?q="+ position.coords.latitude + "," + position.coords.longitude;
+  }
+  };
+  
+  function scrch(){
   var str=document.getElementById("srch").value;
-    str=str.trim();
+  str=str.trim();
   if(str=="")
   location.href="https://github.com/Glitchrex";
-    else if(str=='Google'||str=='google'||str=='GOOGLE'||str=='ggl')
-         location.href="https://www.google.com";
-    else  if(str=='Youtube'||str=='youtube'||str=='YOUTUBE')
-         location.href="https://www.youtube.com";
-   else if(str=='fb'||str=='Facebook'||str=='facebook'||str=='FACEBOOK'||str=='Fb'||str=='FB')
-         location.href="https://www.facebook.com/";
-           else if(str=='ig'||str=='instagram'||str=='Instagram'||str=='INSTAGRAM'||str=="IG"||str=="Ig")
-         location.href="https://www.instagram.com/";
-    else if(str=='whatsapp web'||str=='WHATSAPP'||str=='whatsapp'||str=='Whatsapp')
-         location.href="https://web.whatsapp.com/";
-    else if(str=='Tushar'||str=='tushar'||str==='TUSHAR'||str==='tushar pratap singh'||str==='Tushar pratap singh'||str==='TUSHAR PRATAP SINGH')
+  else if(str=='Google'||str=='google'||str=='GOOGLE'||str=='ggl')
+  location.href="https://www.google.com";
+  else if(str=='Youtube'||str=='youtube'||str=='YOUTUBE')
+  location.href="https://www.youtube.com";
+  else if(str=='fb'||str=='Facebook'||str=='facebook'||str=='FACEBOOK'||str=='Fb'||str=='FB')
+  location.href="https://www.facebook.com/";
+  else if(str=='ig'||str=='instagram'||str=='Instagram'||str=='INSTAGRAM'||str=="IG"||str=="Ig")
+  location.href="https://www.instagram.com/";
+  else if(str=='whatsapp web'||str=='WHATSAPP'||str=='whatsapp'||str=='Whatsapp')
+  location.href="https://web.whatsapp.com/";
+  else if(str=='Tushar'||str=='tushar'||str==='TUSHAR'||str==='tushar pratap singh'||str==='Tushar pratap singh'||str==='TUSHAR PRATAP SINGH')
   location.href="https://github.com/PinkMoon25";
-   else if(str=='to do list'||str=='TO DO LIST'||str=='To do list')
+  else if(str=='to do list'||str=='TO DO LIST'||str=='To do list')
   location.href="https://pinkmoon25.github.io/To-Do-list/";
   else
- location.href="https://www.google.com/search?q="+str;
-};
-
-
-sInput.addEventListener('click',scrch);
-lInput.addEventListener('click',loc);
-
-document.addEventListener('keypress', (e) => {
-    if(e.keycode === 13)
-        sInput.click();
-    });
+  location.href="https://www.google.com/search?q="+str;
+  };
+  
+  const searchBtn = document.getElementById('search');
+  
+  searchBtn.addEventListener('click', scrch);
+  
+  document.addEventListener('keypress',(e) => {
+  if(e.keyCode === 13) {
+  scrch();
+  }
+  });
+  
